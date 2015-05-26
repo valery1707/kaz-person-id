cebe816fb3375570491f72b52b949abe01501e15
Valeriy.Vyrva <valery1707@gmail.com>
2015-05-26 09:37:23 +0600

    Test BasePersonId#equals()

--- src/test/java/name/valery1707/kazPersonId/BasePersonIdTest.java
+++ src/test/java/name/valery1707/kazPersonId/BasePersonIdTest.java
@@ -30,4 +30,37 @@ public class BasePersonIdTest {
 		assertThat(id.isValidCRC()).isFalse();
 		assertThat(id.isValid()).isFalse();
 	}
+
+	private BasePersonId id1() {
+		return new JuridicalPersonId("940140000385");
+	}
+
+	private BasePersonId id2() {
+		return new JuridicalPersonId("950341000052");
+	}
+
+	@Test
+	@SuppressWarnings("EqualsWithItself")
+	public void testEquals() throws Exception {
+		BasePersonId idNull = new JuridicalPersonId(null);
+		BasePersonId id1 = id1();
+		BasePersonId id2 = id2();
+
+		assertThat(idNull).isEqualTo(idNull);
+		assertThat(idNull.equals(idNull)).isTrue();
+		assertThat(idNull).isEqualTo(new JuridicalPersonId(null));
+		assertThat(idNull).isNotEqualTo("");
+		assertThat(idNull).isNotEqualTo(id1);
+		assertThat(idNull).isNotEqualTo(id2);
+
+		assertThat(id1).isNotEqualTo(idNull);
+		assertThat(id1).isEqualTo(id1);
+		assertThat(id1).isEqualTo(id1());
+		assertThat(id1).isNotEqualTo(id2);
+
+		assertThat(id2).isNotEqualTo(idNull);
+		assertThat(id2).isNotEqualTo(id1);
+		assertThat(id2).isEqualTo(id2);
+		assertThat(id2).isEqualTo(id2());
+	}
 }
